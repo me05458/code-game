8a9a012640aa2965bb8fecaeb3ccf38a0b6059f6
me05458 <merlot.eg@gmail.com>
2025-01-12 16:18:45 -0700

    Minor change for comments but mostly to test GPG

--- cards.h
+++ cards.h
@@ -1,1 +1,4 @@
-void renderCards(int cards, int* col, int* num);
+void renderCards(int cards, int* col, int* num); //this renders the cards in hand
+						 //cards - how many cards?
+						 //col - array of colors (should be length cards)
+						 //num - array of nums (should be length cards)
